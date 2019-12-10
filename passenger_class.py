8c2c2946adfbe53f0bba1a704510414388ec16bb
Joseph Pontin <JPontin@spartaglobal.com>
2019-12-10 14:30:28 +0000

    Remove extra code

--- passenger_class.py
+++ passenger_class.py
@@ -1,6 +1,4 @@
 class Passenger:
-
     def __init__(self, name, number):
-        if name and number:
-            self.name = name
-            self.number = number
+         self.name = name
+         self.number = number
